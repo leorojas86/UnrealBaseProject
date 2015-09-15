1ca2e101be67b2254fb45e64e3bc8f25222677b9
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-09-15 09:31:52 -0600

    Implementing Unreal base project.

---
+++ BaseProject/Source/BaseProject/BaseProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BaseProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEPROJECT_API ABaseProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
